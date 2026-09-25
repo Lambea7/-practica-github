348c3cf5ea3eff4561d33596df94bb871ce0481f
Lambea7 <mario.lambea@campus.xaloc.org>
2026-09-25 09:53:08 +0200

    Add files via upload

---
+++ aritmeticas prueba.py
@@ -0,0 +1,18 @@
+variable1=float(input("Introduce el primer numero "))
+variable2=float(input("Introduce el segundo numero "))
+
+print("La suma es: ",variable1+variable2)
+
+print("La resta es: ", variable1-variable2)
+
+print("La multiplicacion es: ", variable1*variable2)
+
+print("La division es: ", variable1/variable2)
+
+print("El modulo es: ", variable1%variable2)
+
+print("La potencia es: ", variable1**variable2)
+
+print("La division entera es: ", variable1//variable2)
+
+
